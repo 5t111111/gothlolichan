3c4558fb091a58b73482712d0ac533de15559ed0
Hirofumi Wakasugi <baenej@gmail.com>
2017-02-19 01:00:00 +0900

    Minor fixes

--- apps/github_webhook/controllers/events/project_card.rb
+++ apps/github_webhook/controllers/events/project_card.rb
@@ -7,11 +7,17 @@ module GithubWebhook::Controllers::Events
     before :check_webhook_secret
 
     def call(params)
-      project_card = GithubWebhook::ProjectCardModel.create(params.get(:project_card))
+      project_card = GithubWebhook::ProjectCardModel.create(
+        action: params.get(:action),
+        project_card: params.get(:project_card),
+        repository: params.get(:repository),
+        sender: params.get(:sender)
+      )
+
       client = Slack::Web::Client.new
       client.chat_postMessage(
         channel: ENV['SLACK_DEFAULT_CHANNEL'],
-        text: "プロジェクトにノートが追加されました [#{project_card.note}]",
+        text: "プロジェクトにノートが追加されました [#{project_card.note}] by #{project_card.username}",
         as_user: true
       )
     end
--- apps/github_webhook/event_models/project_card_model.rb
+++ apps/github_webhook/event_models/project_card_model.rb
@@ -1,19 +1,31 @@
 module GithubWebhook
   class ProjectCardModel
+    ACTION_CREATED = 'created'
+    ACTION_EDITED = 'edited'
+    ACTION_CONVERTED = 'converted'
+    ACTION_MOVED = 'moved'
+    ACTION_DELETED = 'deleted'
+
     private_class_method :new
+    attr_reader :action, :note, :username
 
-    attr_reader :note
-
-    def self.create(payload)
-      payload = payload.to_h
+    def self.create(action:, project_card:, repository:, sender:)
+      action.tapp
+      project_card.tapp
+      repository.tapp
+      sender.tapp
 
       new(
-        note: payload[:note]
+        action: action,
+        note: project_card[:note],
+        username: sender[:login]
       )
     end
 
-    def initialize(note:)
+    def initialize(action:, note:, username:)
+      @action = action
       @note = note
+      @username = username
     end
   end
 end
